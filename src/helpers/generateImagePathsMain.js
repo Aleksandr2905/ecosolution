d2262c0db91998bc3b0e4c37376c47dddb9e85a8
Aleksandr2905 <natalukha@ukr.net>
2024-06-29 21:27:30 +0300

    fix generateImagePathsMain

--- src/helpers/generateImagePathsMain.js
+++ src/helpers/generateImagePathsMain.js
@@ -1,7 +1,6 @@
 export const generateImagePathsMain = (device, format) => {
-  const basePath = '/images';
   return {
-    oneX: `${basePath}/${device}/hero-${device}@1x.${format}`,
-    twoX: `${basePath}/${device}/hero-${device}@2x.${format}`,
+    oneX: `/images/${device}/hero-${device}@1x.${format}`,
+    twoX: `/images/${device}/hero-${device}@2x.${format}`,
   };
 };
